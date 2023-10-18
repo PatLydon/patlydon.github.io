a0f4938c2c80b7d33b1f148ba48677b613806cf2
PatLydon <62838321+PatLydon@users.noreply.github.com>
2023-10-18 22:49:43 +0300

    Add files via upload

---
+++ projects/avto-from-usa/js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+  $('.carousel__inner').slick({
+    slidesToShow: 3,
+    arrows: false,
+    dots:true,
+    responsive: [
+      
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+
+
+    ]
+
+  });
+
+
+});
