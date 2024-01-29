04e155d110a39d009c9043bea2977cb5b478d779
PatLydon <62838321+PatLydon@users.noreply.github.com>
2024-01-29 19:26:34 +0200

    Add files via upload

---
+++ projects/Green-palette/js/main.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.reviews__slider', {
+
+  slidesPerView: 2,
+  spaceBetween: 22,
+
+  navigation: {
+    nextEl: '.reviews__item-next',
+    prevEl: '.reviews__item-prev',
+  },
+
+});
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menuList.classList.toggle('menu--open');
+});
