18bd5da2abd6f3e2e8ff8d293bd8cd28629c5399
PatLydon <62838321+PatLydon@users.noreply.github.com>
2024-02-12 00:08:23 +0200

    Add files via upload

--- projects/asiatour/js/main.js
+++ projects/asiatour/js/main.js
@@ -1,6 +1,15 @@
 
 $(function () {
 
+  $('.burger, .header__top-box body').on('click', function () {
+    $('.burger').toggleClass('burger--active');
+    $('.header__top-box').toggleClass('header__top-box--active');
+    $('body').toggleClass('lock');
+  });
+
+
+
+
   $('.hotel-slider').slick({
     slidesToShow: 3,
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/icons/slider-prev.svg" alt="стрілка вліво"</button>',
@@ -8,15 +17,17 @@ $(function () {
     dots: true,
     responsive: [
       {
-        breakpoint: 841,
+        breakpoint: 992,
         settings: {
-          slidesToShow: 2
+          slidesToShow: 2,
+          autoplay: true,
         }
       },
       {
-        breakpoint: 601,
+        breakpoint: 576,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 1,
+          autoplay: true,
         }
       },
     ]
@@ -27,13 +38,36 @@ $(function () {
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/icons/slider-left.svg" alt="стрілка вліво"</button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/icons/slider-right.svg" alt="стрілка вправо"</button>',
     dots: true,
-    fade:true,
-    autoplay:true,
+    fade: true,
+    autoplay: true,
     autoplaySpeed: 3000,
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+          
+         
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 576,
+    //     settings: {
+  
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+
+    // ]
   });
 
 
   $('select').styler();
-  
+
 
 });
