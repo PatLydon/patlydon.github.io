bf7f7d826324ff39bdcfe127a48962550ccd79b8
PatLydon <62838321+PatLydon@users.noreply.github.com>
2024-09-09 10:09:16 +0300

    Update main.js

--- projects/bitcoin/js/main.js
+++ projects/bitcoin/js/main.js
@@ -1,6 +1,17 @@
-$(function(){
-  $('.burger, .menu a').on('click', function () {
-    $('.menu').toggleClass('menu--active');
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerButton = document.querySelector('.burger');
+  const menuLinks = document.querySelectorAll('.menu a');
+  const menu = document.querySelector('.menu');
 
+  function toggleMenu() {
+    menu.classList.toggle('menu--active');
+  }
+
+  if (burgerButton) {
+    burgerButton.addEventListener('click', toggleMenu);
+  }
+
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', toggleMenu);
+  });
 });
